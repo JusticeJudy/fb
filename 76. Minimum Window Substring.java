f9cedd741d0f9166e091e906d344016036e3b2cd
Wuping <charliu.duke@gmail.com>
2018-02-21 12:19:36 -0500

    Update 76. Minimum Window Substring.java

--- 76. Minimum Window Substring.java
+++ 76. Minimum Window Substring.java
@@ -5,22 +5,36 @@ T = "ABC"
 Minimum window is "BANC".
 
 public String minWindow(String s, String t) {
-    int[] count = new int[128];
-    for (char c : t.toCharArray())
-        count[c]++;
-    String res = "";
-    int start = 0, end = 0, len = t.length(), min = s.length();
-    while (end < s.length()) {
-        if (count[s.charAt(end++)]-- > 0)   len--; // valid
-        while (len == 0) {
-            if (end - start <= min) { // update min & res
-                min = end - start;
-                res = s.substring(start, end);
-            }
-            if (count[s.charAt(start++)]++ == 0)    len++; // make invalid
+    if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+            return "";
         }
-    }
-    return res;
+        int[] counts = new int[256];
+        
+        for (char c : t.toCharArray()) {
+            counts[c]++;   
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int minStart = 0;
+        int total = t.length();
+        for (int left = 0, right = 0; right < s.length(); right++) {
+            if ((counts[s.charAt(right)]--) > 0) {
+                total--;   
+            }
+          // while total count still zero, shrink the window by the left pointer, as long as total is still zero, there's room to shrink the window
+            while (total == 0){
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    minStart = left;
+                }
+                counts[s.charAt(left)]++;
+                if(counts[s.charAt(left)] > 0) {
+                    total++;
+                }
+                left++;
+            }
+        }
+        return (min == Integer.MAX_VALUE)? "": s.substring(minStart, minStart + min);
 }
 
 扣76,   样的是T给的是set,  会有重复,  样瞬秒， 开始问我时间复杂度，我说O(n)他 开始 顿扯，我说T的size 并 影响，最后 他同意

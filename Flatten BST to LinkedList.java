aee47f34f917be182d00c2eb5d7cc8c8d0bc4256
Wuping <charliu.duke@gmail.com>
2018-02-22 16:18:15 -0500

    idk

--- Flatten BST to LinkedList.java
+++ Flatten BST to LinkedList.java
@@ -10,3 +10,24 @@
         prev = root;
         return; 
     }
+
+
+
+// Flatten BST to DLL
+
+    public static TreeNode prev = null;
+    public static void flattenBST2DLL(TreeNode root) {
+        helper(root);
+    }
+    
+    private static  void  helper(TreeNode node) {
+        if (node == null) {
+            return;   
+        }
+        helper(node.left);
+        prev.right = node;
+        node.left = prev;
+        prev = node;
+        helper(node.right);
+        return;
+    }

7d27ac95544dd158eff8875a9054e97d6f22a1d5
Wuping <charliu.duke@gmail.com>
2018-02-22 16:00:39 -0500

    33

--- 33. Search in Rotated Sorted Array.java
+++ 33. Search in Rotated Sorted Array.java
@@ -1,5 +1,23 @@
 33. Search in Rotated Sorted Array
 
+public int search(int[] nums, int target) {        
+    int left = 0, right = nums.length - 1;
+    while (left <= right) { // must find right idx within this loop : =
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)   return mid;
+        else if (nums[mid] < nums[right])
+            if (nums[mid] < target && nums[right] >= target)
+                left = mid + 1;
+            else    right = mid - 1;
+        else
+            if (nums[left] <= target && target < nums[mid])
+                right = mid - 1;
+            else    left = mid + 1;
+    }
+    return -1;
+}
+
+
 public int search(int[] nums, int target) {
     int left = 0, right = nums.length - 1;
     while (left < right) { // find min idx, when left == right return left
@@ -22,19 +40,3 @@ public int search(int[] nums, int target) {
     return -1;
 }
 
-public int search(int[] nums, int target) {        
-    int left = 0, right = nums.length - 1;
-    while (left <= right) { // must find right idx within this loop : =
-        int mid = (left + right) / 2;
-        if (nums[mid] == target)   return mid;
-        else if (nums[mid] < nums[right])
-            if (nums[mid] < target && nums[right] >= target)
-                left = mid + 1;
-            else    right = mid - 1;
-        else
-            if (nums[left] <= target && target < nums[mid])
-                right = mid - 1;
-            else    left = mid + 1;
-    }
-    return -1;
-}

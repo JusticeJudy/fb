e7ad60e7edb49af53512b7d619077dadfff16c6a
Wuping <charliu.duke@gmail.com>
2018-02-22 15:56:49 -0500

    19

---
+++ Number of Bits.java
@@ -0,0 +1,17 @@
+public int hammingWeight1(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>= 1;
+        }
+        return result;
+    }
+    
+    public int hammingWeight(int n) {
+        int result = 0;
+        while (n != 0) {
+            n &= (n - 1);
+            result++;
+        }
+        return result;
+    }

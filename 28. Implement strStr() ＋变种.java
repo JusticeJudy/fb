a5ad1e45668212458b70cdcde51b2310ae674e00
Wuping <charliu.duke@gmail.com>
2018-02-22 15:50:02 -0500

    28

--- 28. Implement strStr() ＋变种.java
+++ 28. Implement strStr() ＋变种.java
@@ -1,3 +1,37 @@
+*******变种*******
+find the first index in haystack that starts with an anagram of needle
+assume only lowercase letters in strings
+
+在a的每个位置截取c=a.substring(i,i+m) ，把c每个字符和次数存进hashmap,然后拿b来对比  时间复杂度是O(m),总时间复杂度是O(mn)
+    
+O(mn) time, O(m) space
+
+public int strStr(String haystack, String needle) {
+    if (needle.length() == 0)    return 0;
+    int m = haystack.length(), n = needle.length();
+    HashMap<String, Integer> map = new HashMap<>();
+    for (int i = 0; i <= m - n; i++) {//we use m - n to reduce time; should be <=, not < !
+        String key = createKey(haystack, i, n);
+        if (!map.containsKey(key)) 
+            map.put(key, i);
+    }
+    String target = createKey(needle, 0, n);
+    return map.containsKey(target) ? map.get(target) : -1;
+}
+
+private String createKey(String s, int start, int length) {
+    int[] count = new int[26]; //see this as O(1) space
+    for (int i = 0; i < length; i++) //O(n) time
+        count[s.charAt(start + i) - 'a']++;
+    String key = "";
+    for (int j = 0; j < count.length; j++) //see this as O(1) time
+        if (count[j] != 0) 
+            key += String.valueOf(count[j]) + String.valueOf((char)('a' + j));
+    return key;
+}
+//http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=130978&fromuid=109727
+
+
 28. Implement strStr()
 
 Time: O(mn)
@@ -53,5 +87,4 @@ private String createKey(String s, int start, int length) {
             key += String.valueOf(count[j]) + String.valueOf((char)('a' + j));
     return key;
 }
-
 //http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=130978&fromuid=109727

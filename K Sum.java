7d9ea51528adc4187c55f71eebc6664a14d2f3ab
Wuping <charliu.duke@gmail.com>
2018-03-01 17:23:19 -0500

    3sum

--- K Sum.java
+++ K Sum.java
@@ -40,26 +40,31 @@ Time: O(n^2)
 public List<List<Integer>> threeSum(int[] nums) {
 	List<List<Integer>> res = new ArrayList<>();
 	Arrays.sort(nums);
-	for (int i = 0; i < nums.length - 2; i++) {
-		if (i > 0 && nums[i] == nums[i - 1]) 	continue;
-		int left = i + 1, right = nums.length - 1;
-		while (left < right) {
-			if (nums[left] + nums[right] == -nums[i]) {
-				res.add(Arrays.asList(nums[i], nums[left], nums[right])); // important! see @ NOTICE
-				while (left < right && nums[left] == nums[left + 1])	left++;
-				while (left < right && nums[right] == nums[right - 1])	right--;
-				left++;
-				right--;
-			} else if (nums[left] + nums[right] < -nums[i]) {
-				while (left < right && nums[left] == nums[left + 1])	left++;
-				left++;
-			} else {
-				while (left < right && nums[right] == nums[right - 1])	right--;
-				right--;
-			}
-		}
-	}
-	return res;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int delta = 0 - nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == delta) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));   
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;                        
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < delta) {
+                    left++;   
+                } else {
+                    right--;   
+                }
+            }
+        }
+       return result;
 }
 
 NOTICE:

877fe470e0d95762a2f6a8d2d99471c15c23c483
Wuping <charliu.duke@gmail.com>
2018-03-01 10:48:48 -0500

    67

--- 67. Add Binary.java
+++ 67. Add Binary.java
@@ -11,16 +11,21 @@ Return "100".
 Solution 1:
 
 public String addBinary(String a, String b) {
+    if (a == null || a.length() == 0) return b;
+    if (b == null || b.length() == 0) return a;
     StringBuilder sb = new StringBuilder();
     int i = a.length() - 1, j = b.length() - 1;
     int sum = 0;
+    int carry = 0;
     while (i >= 0 || j >= 0) {
-        sum /= 2;
+        sum = carry;
         if (i >= 0)     sum += a.charAt(i--) - '0';
         if (j >= 0)     sum += b.charAt(j--) - '0';
-        sb.append(sum % 2);
+        carry = sum/2;
+        sum %= 2;
+        sb.append(sum);
     }
-    if (sum / 2 != 0)   sb.append(sum / 2);
+    if (carry != 0)   sb.append(carry);
     return sb.reverse().toString();
 }
 

72c22b931117be1b4415ad68b6e459940340b048
Wuping <charliu.duke@gmail.com>
2018-03-02 11:22:14 -0500

    update

--- Swap Sort.java
+++ Swap Sort.java
@@ -2,8 +2,13 @@ Swap Sort
 给一个数组，至多允许swap数组里的任意两个数字一次，问是否可以通过swap实现数组排序。
   
 第一种肯定对的解法：另复制一个数组然后排序，跟原来的比较，如果有两个以上数字不一样，不能，两个或两个以下，可以
-第二种：需要先过一遍数组，找第一个大于下面的元素，如果没找到代表已经sorted返回true。找到的话，找这个元素后面的最小的值，然后swap一下看之后的数字是否是sorted的。这样子需要2-pass，时间O(n),不需要sort
-    
+第二种：需要先过一遍数组，找第一个大于下面的元素，如果没找到代表已经sorted返回true。找到的话，找这个元素后面的最小的值，
+    然后swap一下看之后的数字是否是sorted的。这样子需要2-pass，时间O(n),不需要sort
+    // loop over array and find the first instance where an element is smaller than the previous one, and pick the previous one
+     //  (if we can't find such element then the array is already sorted. we return true.
+     //  then we loop over array from the end, and find the first instance where an element is bigger than the one after it.
+     //   and we pick the one after it.
+     // swap this two.  Loop again and see if there any element smaller than previous one.
 public class Solution {
     public swapSort {
         int c1=-1;

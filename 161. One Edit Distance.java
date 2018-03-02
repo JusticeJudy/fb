68b919d5afa08669415cd57aa1e798557e667353
Wuping <charliu.duke@gmail.com>
2018-03-02 11:09:04 -0500

    update 161

--- 161. One Edit Distance.java
+++ 161. One Edit Distance.java
@@ -18,3 +18,57 @@ public boolean isOneEditDistance(String s, String t) {
 }
 
 原题 但是不能用substring ====>>>   要一个字符一个字符比较
+
+
+
+public boolean isOneEditDistance(String s, String t) {
+    if (s == null || t == null || s.equals(t) || Math.abs(s.length() - t.length()) > 1) {
+        return false;
+    }
+    
+    int m = s.length();
+    int n = t.length();
+    char[] sc = s.toCharArray();
+    char[] tc = t.toCharArray();
+    
+    if (m == n) {
+        return checkCase1(sc, tc);
+    } else if (m > n) {
+        return checkCase23(sc, tc);
+    } else {
+        return checkCase23(tc, sc);
+    }
+}
+private boolean checkCase1(char[] sc, char[] tc) {
+    int m = sc.length;
+    int n = tc.length;
+    int i = 0, j = 0, count = 0;
+    while (i < m && j < n) {
+        if (sc[i++] != tc[j++]) {
+            count++;
+        }
+    }
+    if (count > 1) {
+        return false;
+    }
+    return true;
+}
+
+private boolean checkCase23(char[]sc, char[] tc) {
+    int m = sc.length;
+    int n = tc.length;
+    int i = 0, j = 0, count = 0;
+    while (i < m && j < n) {    // only one chanse of discrepency and skip one char
+        if (sc[i] != tc[j]) {
+            i++;
+            count++;
+        } else {
+            i++;
+            j++;
+        }
+    }
+    if (count > 1) {
+        return false;
+    }
+    return true;
+}

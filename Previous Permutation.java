6d138506b141373455ae33403efa8a0fea634082
Wuping <charliu.duke@gmail.com>
2018-03-02 15:05:07 -0500

    update

--- Previous Permutation.java
+++ Previous Permutation.java
@@ -1,19 +1,27 @@
 //https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 
 Previous Permutation
-
+// To find the previous permutation, we mean finding the the number that is right in front of it in an ascending order, just
+ 	// a tad smaller. 
 2764135 （找最后一个逆序41）
+// The above example, we loop from the end of array. Find the dip of the trend, right when the digits start to pick up.
+ // which is 4 in this case, and we want to swap this with a digit that is most closely smaller than 4, which is 3.
+ // after we swop it, we reverse the order of the array from the original 4's position.
 2764135 （找4后面比4小的最后一个数3）
 2763145 （交换3,4的位置）
 2763541 （把3后面的1,4,5反转）
 
 public void previousPermuation(int[] nums) {
 	if (nums.length < 2)    return;
+	// Index of the number before first dip 
     int firstLarger = nums.length - 2;
     while (firstLarger >= 0) {
         if (nums[firstLarger] > nums[firstLarger + 1])    break;
         firstLarger--;
     }
+    
+    // if index equals -1, the number itself is the smallest in the ascending order, so we reverse whole array.
+    	// ask them first, what to do with the number that is front of the queue already
     if (firstLarger == -1) { 
         reverse(nums, 0, nums.length - 1);
         return;
@@ -43,18 +51,26 @@ private void reverse(int[] nums, int left, int right) {
 31. Next Permutation
 // https://leetcode.com/problems/next-permutation/
 
+// To find the previous permutation, we mean finding the the number that is right after it in an ascending order, just a tad
+  // larger.
 2763541 （找最后一个正序35）
+// The above example, we loop from the end of array. Find the peak of the trend, right when the digits start to climb down.
+ // which is 3 in this case, and we want to swap this with a digit that is most closely bigger than 3, which is 4.
+ // after we swop it, we reverse the order of the array from the original 3's position.
 2763541 （找3后面比3大的最后一个数4）
 2764531 （交换3,4的位置）
 2764135 （把4后面的5,3,1反转）
 
 public void nextPermutation(int[] nums) {
     if (nums.length < 2)    return;
+    // find the index of the number right before the first peak
     int firstSmaller = nums.length - 2;
     while (firstSmaller >= 0) {
         if (nums[firstSmaller] < nums[firstSmaller + 1])    break;
         firstSmaller--;
     }
+    // if index equals -1, the number itself is the biggest in the ascending order, so we reverse whole array.
+    	// ask them first, what to do with the number that is at the end of the queue already
     if (firstSmaller == -1) {
         reverse(nums, 0, nums.length - 1);
         return;

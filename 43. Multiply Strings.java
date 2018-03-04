57c4ca4702ee6cd4cdbd56331e24b38b27d0b585
Wuping <charliu.duke@gmail.com>
2018-03-04 14:00:38 -0500

    update

--- 43. Multiply Strings.java
+++ 43. Multiply Strings.java
@@ -7,6 +7,37 @@ Both num1 and num2 contains only digits 0-9.
 Both num1 and num2 does not contain any leading zero.
 You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
+我们以289*785为例
+// http://www.cnblogs.com/TenosDoIt/p/3735309.html
+
+public String multiply(String num1, String num2) {
+        if(num1 == null || num2 == null) {
+            return "";   
+        }
+        
+        int n1 = num1.length(), n2 = num2.length();
+        int[] digits = new int[n1 + n2];
+        int k = n1 + n2 - 2, carry = 0;
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                digits[k - i - j] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+            }
+        }
+        for (int i = 0; i < n1 + n2; i++) {
+            digits[i] += carry;
+            carry = digits[i]/10;
+            digits[i] %= 10;
+        }
+        
+        int i = n1 + n2 - 1;
+        while (i >= 0 && digits[i] == 0) i--;
+        if (i < 0) return "0";
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0) {
+            sb.append(digits[i--]);
+        }
+        return sb.toString();
+}
 
 public String multiply(String num1, String num2) {
     int m = num1.length(), n = num2.length();

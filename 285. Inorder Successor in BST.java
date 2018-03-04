a22f6f672ead47d6e05cab7d3c0f3eb76fbb8c0e
Wuping <charliu.duke@gmail.com>
2018-03-04 16:42:38 -0500

    update

--- 285. Inorder Successor in BST.java
+++ 285. Inorder Successor in BST.java
@@ -7,8 +7,11 @@ TreeNode没有parent引用：
 下面两种解法本质一样，都是充分利用BST的性质。
 Solution 1: iteration (4ms)
 // The idea is to compare root's value with p's value if root is not null, and consider the following two cases:
-// root.val > p.val. In this case, root can be a possible answer, so we store the root node first and call it res. However, we don't know if there is anymore node on root's left that is larger than p.val. So we move root to its left and check again.
-// root.val <= p.val. In this case, root cannot be p's inorder successor, neither can root's left child. So we only need to consider root's right child, thus we move root to its right and check again.
+// 1 .root.val > p.val. In this case, root can be a possible answer, so we store the root node first and call it res. 
+  // However, we don't know if there is anymore node on root's left that is larger than p.val. So we move root to its 
+  // left and check again.
+// 2. root.val <= p.val. In this case, root cannot be p's inorder successor, neither can root's left child. 
+  // So we only need to consider root's right child, thus we move root to its right and check again.
 // We continuously move root until exhausted. To this point, we only need to return the res in case 1.'
 
 public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
